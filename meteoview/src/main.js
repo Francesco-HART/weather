a6662f0eb1c32a99703dad5fbef0936dd2ff562a
Corentin Vallois <vallois@macbook-pro.edu.sir>
2020-09-30 14:35:05 +0200

    editor add to git ignore

--- meteoview/src/main.js
+++ meteoview/src/main.js
@@ -1,20 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import {library} from '@fortawesome/fontawesome-svg-core'
 import {faWind, faTint, faThermometerHalf, faLongArrowAltDown} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import {CardPlugin} from 'bootstrap-vue'
+import App from './App.vue'
+
 
 Vue.use(CardPlugin)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-library.add(faWind,faTint,faThermometerHalf,faLongArrowAltDown)
+library.add(faWind, faTint, faThermometerHalf, faLongArrowAltDown)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
