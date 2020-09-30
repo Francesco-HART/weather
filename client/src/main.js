f99faac5d2410798a4c77a28c9811fa438ff1feb
Corentin Vallois <vallois@macbook-pro.edu.sir>
2020-09-30 15:56:21 +0200

    maj

--- client/src/main.js
+++ client/src/main.js
@@ -1,21 +1,13 @@
-import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faWind, faTint, faThermometerHalf } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { BootstrapVue, IconsPlugin, CardPlugin } from 'bootstrap-vue';
-import App from './App.vue';
-import store from './store/index';
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
 
-Vue.use(CardPlugin, BootstrapVue, IconsPlugin);
-library.add(faWind, faTint, faThermometerHalf);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount('#app')
