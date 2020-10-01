1770b911c540dc9e223e80ab0b734adb47ac24f5
Corentin Vallois <vallois@macbook-pro.edu.sir>
2020-10-01 17:02:08 +0200

    local-storage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
 Vue.use(Vuex)
-
+const vuexLocal = new VuexPersistence({storage: window.localStorage,supportCircular: true,})
 export const store = new Vuex.Store({
-  state: {
-    citiesData:[],
-    citiesName: ['paris', 'cergy']
-  },
-  getters:{
-    citiesData: state => state.citiesData,
-    citiesName: state => state.citiesName
-  },
-  mutations:{
-    addNewCity(state, data) {
-        state.citiesData.push(data)
+    state: {
+        citiesData: [],
+        citiesName: ['paris', 'cergy']
     },
-    addCityName(state, name) {
-      console.log(state.citiesName)
-      if(!state.citiesName.includes(name.toLowerCase()))
-      state.citiesName.push(name.toLowerCase())
-    }
-  },
-  actions: {
-  },
-  modules: {
-  }
+    getters: {
+        citiesData: state => state.citiesData,
+        citiesName: state => state.citiesName
+    },
+    mutations: {
+        addNewCity(state, data) {
+            state.citiesData.push(data)
+        },
+        addCityName(state, name) {
+            console.log(state.citiesName)
+            if (!state.citiesName.includes(name.toLowerCase()))
+                state.citiesName.push(name.toLowerCase())
+        }
+    },
+    actions: {},
+    modules: {},
+    plugins: [vuexLocal.plugin]
 })
