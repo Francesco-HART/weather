54a53caa0d4590999861fb0b47c8d912ae60f9bc
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2020-10-01 21:44:43 +0200

    correctif of tab

--- client/src/components/TabWather.vue
+++ client/src/components/TabWather.vue
@@ -2,34 +2,20 @@
 <div>
   <h1>{{data.city.name}}</h1>
   <div>
-    <b-tabs content-class="mt-3">
+   <!-- <b-tabs content-class="mt-3">
       <b-tab v-for="(tabline, index) in data.list" :key="index" title="météo sur 5 jour toutes les 3h"
        active>
         <template v-if="index > 0" slot="title">
           {{tabline.dt | moment("LLLL")}}
         </template>
-        <div v-if="index > 0">
-          <p>
-            Température ressenti : {{tabline.main.feels_like | celsiusOrFahrenheit(isCelsius)}}
-          </p>
-          <p>
-            Température max : {{ tabline.main.temp_max }}
-          </p>
-          <p>
-            Température min : {{ tabline.main.temp_min }}
-          </p>
-          <p>
-            Température : {{ tabline.main.temp }}
-          </p>
-          <p>
-            Humidité : {{tabline.main.humidity}} %
-          </p>
-          <p>
-            Vent :{{tabline.wind.speed}} m/s
-          </p>
-        </div>
-      </b-tab>
-    </b-tabs>
+        <div v-if="index > 0">-->
+
+
+          <b-table striped hover :items="items"></b-table>
+
+    <!--</div>
+  </b-tab>
+</b-tabs>-->
   </div>
 </div>
 </template>
@@ -38,18 +24,30 @@
 export default {
 name: "TabWather",
   props : {
-    data: Array,
+    data: Object,
   },
   data() {
     return {
       isCelsius: true,
+      items: [],
     }
   },
+
   filters: {
     celsiusOrFahrenheit: function(value, isCelsius) {
       return isCelsius ? value.toFixed(1) + "°C" : "" + (value * 1.8000 + 32.00).toFixed(1) + "°F";
     }
   },
+  created: function() {
+    for (let i = 1 ; i < this.data.list.length ;  i++){
+      this.items.push({temperature_ressentie : this.data.list[i].main.feels_like.toFixed(1),
+          temperature_max : this.data.list[i].main.temp_max,
+          temperature_min : this.data.list[i].main.temp_min,
+          temperature : this.data.list[i].main.temp,
+          humidity : this.data.list[i].main.humidity,
+          vent : this.data.list[i].wind.speed});
+    }
+  }
 }
 </script>
 
